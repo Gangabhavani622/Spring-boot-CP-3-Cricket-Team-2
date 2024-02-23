f507e924bd3fdc7780b413a13c3137dfdb9c3bbd
Gangabhavani622 <115251057+Gangabhavani622@users.noreply.github.com>
2024-02-23 14:01:26 +0530

    Update PlayerController.java

--- src/main/java/com/example/player/controller/PlayerController.java
+++ src/main/java/com/example/player/controller/PlayerController.java
@@ -10,6 +10,8 @@
 package com.example.player.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.*;
 
 import com.example.player.model.Player;
@@ -17,7 +19,8 @@ import com.example.player.service.PlayerH2Service;
 
 @RestController
 public class PlayerController {
-    PlayerH2Service playerH2Service = new PlayerH2Service();
+    @Autowired
+    public PlayerH2Service playerH2Service;
 
     @GetMapping("/players")
     public ArrayList<Player> getPlayersList() {
